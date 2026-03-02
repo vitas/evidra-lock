c02dcd58aeaf4eb795be919b89caea182dd1da29
Vitaliy Ryumshyn <vr@samebits.com>
2026-03-03 00:10:45 +0100

    engine v2 add ui tests

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version is the build/runtime version string for Evidra binaries.
-	Version = "0.1.0-dev"
+	Version = "0.2.0-dev"
 	// Commit describes the revision or commit hash used to build the binary.
 	Commit = "dev"
 	// Date stores the build timestamp.
